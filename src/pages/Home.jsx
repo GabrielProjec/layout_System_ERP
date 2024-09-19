ac69e43bd49b13adf303950239844b33aaf5f6ef
GabrielProjec <gabriel.acesso3@gmail.com>
2024-09-19 17:26:35 -0300

    iconsProject

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import DashboardCards from '../components/DashboardCards'
 // ICONS
 import { PiStudent } from "react-icons/pi";
 import { MdOndemandVideo } from "react-icons/md";
+import { FaCode } from "react-icons/fa";
 
 const Home = () => {
   return (
@@ -16,7 +17,7 @@ const Home = () => {
       <div className='p-4 w-full flex flex-wrap items-center justify-center gap-4'>
         <DashboardCards title="Total Alunos" number="2000" icon={<PiStudent />} bgBorder='border-[rgb(32,45,105)] dark:border-blue-400'  />
         <DashboardCards title="Total Cursos" number="4" icon={<MdOndemandVideo />} bgBorder='border-red-400 dark:border-blue-400'  />
-        <DashboardCards title="Total Projetos" number="100" icon={<PiStudent />} bgBorder='border-indigo-400 dark:border-blue-400'  />
+        <DashboardCards title="Total Projetos" number="100" icon={<FaCode />} bgBorder='border-indigo-400 dark:border-blue-400'  />
         
       </div>
     </div>
